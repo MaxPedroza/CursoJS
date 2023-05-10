2d740ab3ebaeb2c94f6abf1f0f319cf3c7f2f256
Max Pedroza <107723446+MaxPedroza@users.noreply.github.com>
2023-05-10 16:57:13 -0300

    Atualização

--- Aulas JS/Execucao.js
+++ Aulas JS/Execucao.js
@@ -274,5 +274,59 @@ console.log(num2 ** (1/2));// raiz quadrada
 //  esse tipo de resposta se da quando o resultado fica muito grande 
 
 
+// == Aula 18 == Funções
+
+
+    //Exemplo 1
+/*
+function soma (num1,num2) {
+
+    const resultado = num1+num2;
+    return resultado;           // abaixo de Return nada mais era executado
+    
+}
+
+const resultado = soma(2,2)     // A variavel resultado nao tem nada a ver com a variavel resultado dentro da função 
+console.log(soma(2, 2));
+console.log(soma(4, 2));
+console.log(soma(2, 6));
+console.log(resultado);
+*/
+
+    // Exemplo 2
+/*
+function soma (num1 = 1,num2 = 1) {            // Caso eu defina um valor para as variaveis, elas poderao ser alteradas mais a frente, caso isso nao ocorra elas tem o valor definido no inicio 
+
+    const resultado = num1+num2;
+    return resultado;                           // abaixo de Return nada mais era executado
+    
+}
+
+const resultado = soma(2)                       // Aqui apenas uma variavel assume o valor 2
+
+console.log(resultado);
+*/
+
+    // Exemplo 3 de criação de Function
+/*
+const raiz = function(n) {                      //declarada como constante
+
+    return n ** 0.5;
+};
+
+console.log(raiz(9));
+*/
+
+// Exemplo 4 de criação de Function
+/*
+
+const raiz =  n => n ** 0.5;                  // Arrow Function =>
+
+
+console.log(raiz(9));
+*/
+
+
+// == Aula 19  == Objetos
 
 
--- Aulas JS/conteudoAnotacoes.js
+++ Aulas JS/conteudoAnotacoes.js
@@ -267,6 +267,8 @@ console.log(umaString.charAt(4)); // outra forma de exibir a posição do caract
 
 // == Auula 15 == Math
 
+// == Aula 16 == Exercicio com mumeros e texto utilizando js no html vide exercicio 16
+
 /*
 //let numero = prompt('Digite um número');                                                          // Inseri uma variavel que Cria um input para digitar uma informação
 //numero = Number(numero);                                                                          // Converte a string do input para number
@@ -287,3 +289,52 @@ texto.innerHTML += `<p>Seu número arredondado pra baixo é: <i>${Math.floor(num
 texto.innerHTML += `<p>Seu número arredondado pra cima é: <i>${Math.ceil(numero)}</i>.</p>`;
 texto.innerHTML += `<p>Seu número com duas casas decimais é: <i>${numero.toFixed(2)}</i>.</p>`;
 */
+
+// == Aula 17 == Arrays
+/*
+//Exemplo de array
+
+//            01234567891011
+//const nome = 'Max Pedroza'                  //Variavel com um unico nome
+
+//               0      1       2
+const nomes = ['Max', 'Elen', 'Nina'];                                               // Variavel utilizando array com varios nomes
+//console.log(nomes);                                                                // exibindo array
+
+//console.log(nome [0]);                                                             // exibe o indice 0 da string, lembrando que o primeiro indice começa com zero    
+//console.log(nomes [0]);                                                            // exibe o indice 0 da array, mesmo caso que da string
+
+//nomes[0] = 'Max Pedroza';                                                          // Alterando informação do indice 0 da array
+
+//console.log(nomes);                                                                // Exibe(array completa) a ultima alteração
+//console.log(nomes[0])                                                              // Exibe(array indice 0) a ultima alteração
+
+//nomes [3] = 'Max'                                                                  // Adiciono conteudo ao indice 3, que antes nao existia
+//console.log(nomes);                                                                // Exibe(array completa) a ultima alteração
+
+
+
+nomes[nomes.length] = 'Rivaldo';                                                     // Outra forma de adicionar (Adicionar no fim)
+nomes[nomes.length] = 'Eliude';                                                      //          --//--
+
+//nomes.push('café');                                                                // Outra forma de adicionar (Adiciona no fim)
+
+//nomes.unshift('Max Pedroza');                                                      // Adicionar no inicio da array
+
+//const removeUltimo = nomes.pop();                                                  // Retira o ultimo da lista
+//const removePrimeiro = nomes.shift();                                              // Retira o primeiro da lsita
+
+delete nomes [1];                                                                    // Deleta um indice, usando delete o indice fica vazio
+
+console.log(`Esse é o conteudo da sua lista: ${nomes} `);                                                                  // Exibe o array
+console.log(`O tamanho do array é de: ${nomes.length} índices.`);                    // Exibe o tamanho do Array
+//console.log(removePrimeiro);
+//console.log(removeUltimo);
+
+//console.log(nomes.slice(0,-2));                                                    // Exibe parte da array, omitindo os dois ultimos nomes da lista (-2)
+//console.log(typeof nomes);                                                           // Exibe o que é a variavel (object)
+//console.log(nomes instanceof Array);                                                 // Exibe se a variavel é um array (true ou false)
+*/
+
+// == Aula 18 == Funções
+
